a9abe37e08dac795da2f1107b0884d138d67eaa1
Manjeet <manjeet.bluethink@gmail.com>
2023-05-10 06:39:35 -0400

    Xbdl documents and git crediantials

---
+++ command.sh
@@ -0,0 +1,10 @@
+bin/magento s:up
+bin/magento s:d:c
+bin/magento s:s:d en_US -f
+bin/cli bash
+npm --prefix vendor/gorilla/m2-frontend-tools/ run styles
+exit
+bin/magento c:f
+
+bugfix/59132-ssopopup-not-working-on-imagine-your-perfect-combination
+release/magento-upgrade-2-4-5-tengan
